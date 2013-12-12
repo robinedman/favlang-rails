0f68e66b16290d4087d0dbc24296db8b4e137a9d
robin <robin.edman@gmail.com>
2013-12-12 19:34:47 +0000

    Error handling wasn't implemented at the controller level.

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,11 +3,18 @@ require 'favourite_language_guesser'
 class LandingController < ApplicationController
   def guess
     @lg = FavouriteLanguageGuesser.new
-    render(json: 
-      {
-        favourite: @lg.favourite_language(params[:github_username]),
-        username: params[:github_username]
-      }
-    )
+
+    begin
+      favourite_language = @lg.favourite_language(params[:github_username])
+    rescue GitHubAPI::GitHubAPIError => e
+      render(text: e.message, status: 404)
+    else
+      render(json: 
+        {
+          favourite: @lg.favourite_language(params[:github_username]),
+          username: params[:github_username]
+        }
+      )
+    end
   end
 end
