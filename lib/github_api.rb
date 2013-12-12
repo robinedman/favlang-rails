c6add4f1166c3f7367567960e9a994db4d09a689
robin <robin.edman@gmail.com>
2013-12-12 19:35:03 +0000

    Updated github_api to the latest version.

--- lib/github_api.rb
+++ lib/github_api.rb
@@ -47,12 +47,28 @@ module GitHubAPI
     end
   end
 
+  class UserHasNoReposError < GitHubAPIError
+    attr_reader :username
+
+    def initialize(username)
+      @username = username
+    end
+    def self.no_repos?(response)
+      response.is_a?(Array) && response.empty?
+    end
+
+    def message
+      "The user #{@username} has no repositories."
+    end
+  end
+
   def self.get_user_repos(username)
       uri = URI("https://api.github.com/users/#{username}/repos")
       response = JSON.parse(Net::HTTP.get(uri))
       
       raise UserNotFoundError.new(username) if UserNotFoundError.not_found?(response)
       raise APILimitError.new if APILimitError.api_limit?(response)
+      raise UserHasNoReposError.new(username) if UserHasNoReposError.no_repos?(response)
 
       response
     end
